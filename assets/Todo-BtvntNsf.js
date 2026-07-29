9bd47a93d9b9ae8e1b11a3f6f780db1bddac5bf1
Yuuuu <yuxuanf422@gmail.com>
2026-07-29 11:36:51 +0800

    Updates

--- assets/Todo-BtvntNsf.js
+++ assets/Todo-BtvntNsf.js
@@ -1,4 +1,4 @@
-import{r as s,j as e,g as H,R as V}from"./index-D0BDWKa1.js";import{M as O}from"./Modal-D0BZCki2.js";import{B as _}from"./BackButton-B9d1jSf1.js";import{I as q}from"./ImageWithLoader-D93u01bk.js";function B({item:n,onComplete:d,onEdit:x,onDelete:u}){const[o,k]=s.useState(!1),N=a=>{a.stopPropagation(),k(!0),setTimeout(()=>d(n.id),300)},w=a=>{a.stopPropagation(),u(n.id)};return e.jsxs("div",{className:`
+import{r as s,j as e,g as H,R as V}from"./index-D5XUwTPR.js";import{M as O}from"./Modal-DnSmXcG5.js";import{B as _}from"./BackButton-t7gk90OY.js";import{I as q}from"./ImageWithLoader-DZZkXuXt.js";function B({item:n,onComplete:d,onEdit:x,onDelete:u}){const[o,k]=s.useState(!1),N=a=>{a.stopPropagation(),k(!0),setTimeout(()=>d(n.id),300)},w=a=>{a.stopPropagation(),u(n.id)};return e.jsxs("div",{className:`
         flex items-center gap-2 px-3 py-3 rounded-2xl transition-all duration-300
         ${o?"opacity-0 -translate-x-8 scale-95":"opacity-100"}
         ${n.type==="urgent"?"bg-pink/10":"bg-gray-100"}
